4618613a66ecbebed09c5ce0c9409270c176621e
Mykola Smith <mykolasmith@gmail.com>
2015-06-17 20:41:01 -0400

    clean up debug osc helper

--- debug_osc.py
+++ debug_osc.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-from time import sleep
+import time
 
 from OSC import OSCServer
 
@@ -8,24 +8,9 @@ ip = socket.gethostbyname(socket.gethostname())
 server = OSCServer( (ip, 7000) )
 
 def callback(path, tags, args, source):
-    # which user will be determined by path:
-    # we just throw away all slashes and join together what's left
     print path, tags, args, source
 
-# user script that's called by the game engine every frame
-def each_frame():
-    # clear timed_out flag
-    server.timed_out = False
-    # handle all pending requests then return
-    while not server.timed_out:
-        server.handle_request()
-
-# simulate a "game engine"
 while True:
-    # do the game stuff:
     server.noCallback_handler = callback
-    sleep(1)
-    # call user script
-    each_frame()
-
-server.close()
+    time.sleep(1)
+    server.handle_request()
