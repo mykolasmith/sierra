aa138bac87bb807ac1f9b0612a0bcb47a5ecfa5a
Mykola Smith <mykolasmith@gmail.com>
2015-08-11 11:20:04 -0400

    num_frames only needs to be declared once

--- animation/motion_tween.py
+++ animation/motion_tween.py
@@ -8,6 +8,8 @@ class MotionTween(Animation):
         
         self.refresh_params()
         self.trail = xrange(0, int(self.normalize(self.trail_length, 1, 60)))
+        self.num_frames = self.length + len(self.trail)
+        
         if self.duration > 0:
             self.speed = self.duration
         else:
@@ -15,18 +17,18 @@ class MotionTween(Animation):
         
     def run(self, deltaMs):
         self.pixels[...] = 0
-        self.num_frames = self.length + len(self.trail)
         
         if self.refresh_enabled:
             self.refresh_params()
         
         frame = int(round(math.sin(deltaMs / self.speed) * self.num_frames))
+        if frame == self.num_frames:
+            self.done = True
+            return
+        
         rgb = self.hsb_to_rgb(self.hue,self.saturation,self.brightness)
         for offset in reversed(self.trail):
             factor = 1 - (float(offset) / len(self.trail))
             px = frame-offset
             if 0 <= px < self.length:
                 self.pixels[px] = rgb * factor
-                
-        if frame == self.num_frames:
-            self.done = True
