b0a667acdb48fd478308b6de3de8c898639c4100
Mykola Smith <mykola@Mykolas-MBP.fios-router.home>
2015-11-09 22:43:03 -0500

    decay and experiment w/ overlapping bars

--- animation/positional.py
+++ animation/positional.py
@@ -3,18 +3,18 @@ from base import Animation
 import math
 
 class Positional(Animation):
-    
+
     def __init__(self, config):
         super(Positional, self).__init__(config)
-        
+
         # Look at the range of notes assigned to this animations
         # In order to determine the min and max
         self.min = min(self.notes)
         self.max = max(self.notes)
-        
+
         # The min and max can then be used to deterine the correct
         # position for a set of LEDs, depending on the note pressed.
-        
+
         self.width = round((1. / (self.max - self.min)) * self.length)
         self.start = (self.msg.note - self.min) * self.width
         self.end = self.start + self.width
@@ -24,20 +24,20 @@ class Positional(Animation):
         width = self.find_width(deltaMs)
         if width == 1.0 or self.oscillation == 0.0:
             self.refresh_params()
-        
+
         if deltaMs < self.attack:
             self.brightness = deltaMs / self.attack
         else:
             self.brightness = 1.0
-        
+
         self.draw(width)
-            
+
     def find_width(self, deltaMs):
         if self.oscillation != 0:
             oscillation = self.normalize(self.oscillation, 0, 10)
-            return round(self.width * abs(math.sin(deltaMs * oscillation)))
+            return 3 * round(self.width * abs(math.sin(deltaMs * oscillation)))
         return self.width
-            
+
     def draw(self, width):
         try:
             rgb = self.hsb_to_rgb(self.hue, self.saturation, self.brightness)
@@ -48,3 +48,11 @@ class Positional(Animation):
                 self.pixels[self.midpoint-px] = rgb * factor
         except IndexError:
             pass
+
+    def off(self, deltaMs):
+        width = self.find_width(deltaMs)
+        if deltaMs < self.decay:
+            self.brightness = 1 - (deltaMs / self.decay)
+        else:
+            self.brightness = 0.0
+        self.draw(width)
